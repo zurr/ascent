f1a55aac632c2e155777cbeeb3eb28a3341e2083
grep <grep@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-06-03 07:21:37 +0000

    - Fixed % damage modifiers, provided by TheNecromancer. - updated svn rev

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3757,13 +3757,17 @@ void Spell::SpellEffectDummyMelee(uint32 i) // Normalized Weapon damage +
 		return;
 
 	// fix double attack on backstab etc.
-	if(m_spellInfo->Effect[1] == SPELL_EFFECT_WEAPON_PERCENT_DAMAGE ||
-		m_spellInfo->Effect[2] == SPELL_EFFECT_WEAPON_PERCENT_DAMAGE)
+	if(m_spellInfo->Effect[1] == SPELL_EFFECT_WEAPON_PERCENT_DAMAGE)
 	{
 		// rogue ambush etc
-		add_damage = damage * 1.5;
+		add_damage = damage * (m_spellInfo->EffectBasePoints[1]+1)/100;
 		return;
 	}
+	else if(m_spellInfo->Effect[2] = SPELL_EFFECT_WEAPON_PERCENT_DAMAGE)
+	{
+		add_damage = damage * (m_spellInfo->EffectBasePoints[2]+1)/100;
+		return;
+	}		
    
 	u_caster->Strike(unitTarget,GetType() == SPELL_TYPE_RANGED ? SPELL_TYPE_RANGED:0,m_spellInfo,damage,0,0);
 
