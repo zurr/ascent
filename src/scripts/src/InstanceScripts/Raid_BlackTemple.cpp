f464bbc4040c782ab8c52221392283f8e90e87be
M4ksiu <M4ksiu@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-06-29 11:05:52 +0000

    Changes in Black Temple raid script: * Fixed Akama wps. Now it will go correctly from door to Illidan position. + Added dialog between Akama and Illidan after which Illidan is attackable (but Akama doesn't join fight - must find way to change that) + Added more stuff to Illidan. Now phase 2 works, but it doesn't contains all it's spells and other needed stuff. What I added is spawning 2 Flames of Azzinoth by pushing them into world (spell way doesn't work yet =( ) and casting blue fireball spell by Illidan which doesn't make dmg :O (no idea why, with method "TARGET_ATTACKING" it wasn't even casted, with "TARGET_DESTINATION" it is casted, but no dmg so I left visual effect :P) + Added connection between phase 1 and phase 3 (now you must take care of Flames of Azzinoth before phase 3 will start) + Added Flame of Azzinoth script without spells to connect phases
    TO DO: Check if there is timer change avaible (I found why it was only used when InCombat :D) and if it is synchronizate trasformation and it's reversion. Also add more spells to phases (mostly phase 2). Add Phase 5 with Maiev appearing etc. Add death scene. (anything was skiped?)

--- src/scripts/src/InstanceScripts/Raid_BlackTemple.cpp
+++ src/scripts/src/InstanceScripts/Raid_BlackTemple.cpp
@@ -13,11 +13,128 @@
 
 // Additional stuff
 uint32 m_phase;
-int AKAMA_DIALOG;
+uint32 AKAMA_DIALOG;
+uint32 FoADeaths;
 
 #define WALK 0
 #define RUN 256
 #define FLY 768
+
+// Flame of AzzinothAI
+
+#define CN_FLAME_OF_AZZINOTH 22997	
+
+class FlameOfAzzinothAI : public CreatureAIScript
+{
+public:
+	ADD_CREATURE_FACTORY_FUNCTION(FlameOfAzzinothAI);
+	//SP_AI_Spell spells[0];	// Not used yet so disabled to prevent illegal zero array error (disabled all spell cast related functions)
+	//bool m_spellcheck[0];
+
+    FlameOfAzzinothAI(Creature* pCreature) : CreatureAIScript(pCreature)
+    {
+/*
+		nrspells = 0;
+		for(int i=0;i<nrspells;i++)
+		{
+			m_spellcheck[i] = false;
+		}
+
+		spells[0].info = sSpellStore.LookupEntry(BOMB);
+		spells[0].targettype = TARGET_DESTINATION;
+		spells[0].instant = false;
+		spells[0].cooldown = 10;
+		spells[0].perctrigger = 0.0f;
+		spells[0].attackstoptimer = 1000;
+*/
+    }
+    
+    void OnCombatStart(Unit* mTarget)
+    {
+		//CastTime();
+		RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
+    }
+/*
+	void CastTime()
+	{
+		for(int i=0;i<nrspells;i++)
+			spells[i].casttime = spells[i].cooldown;
+	}
+*/
+	void OnTargetDied(Unit* mTarget)
+    {
+    }
+
+    void OnCombatStop(Unit *mTarget)
+    {
+		//CastTime();
+        _unit->GetAIInterface()->setCurrentAgent(AGENT_NULL);
+        _unit->GetAIInterface()->SetAIState(STATE_IDLE);
+        RemoveAIUpdateEvent();
+    }
+
+    void OnDied(Unit * mKiller)
+    {
+		//CastTime();
+       RemoveAIUpdateEvent();
+	   FoADeaths--;
+    }
+
+    void AIUpdate()
+	{
+		/*float val = sRand.rand(100.0f);
+		SpellCast(val);*/
+    }
+/*
+	void SpellCast(float val)
+	{
+        if(_unit->GetCurrentSpell() == NULL && _unit->GetAIInterface()->GetNextTarget())
+        {
+			float comulativeperc = 0;
+		    Unit *target = NULL;
+			for(int i=0;i<nrspells;i++)
+			{
+				spells[i].casttime--;
+				
+				if (m_spellcheck[i])
+				{					
+					spells[i].casttime = spells[i].cooldown;
+					target = _unit->GetAIInterface()->GetNextTarget();
+					switch(spells[i].targettype)
+					{
+						case TARGET_SELF:
+						case TARGET_VARIOUS:
+							_unit->CastSpell(_unit, spells[i].info, spells[i].instant); break;
+						case TARGET_ATTACKING:
+							_unit->CastSpell(target, spells[i].info, spells[i].instant); break;
+						case TARGET_DESTINATION:
+							_unit->CastSpellAoF(target->GetPositionX(),target->GetPositionY(),target->GetPositionZ(), spells[i].info, spells[i].instant); break;
+					}
+
+					if (spells[i].speech != "")
+					{
+						_unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, spells[i].speech.c_str());
+						_unit->PlaySoundToSet(spells[i].soundid); 
+					}
+
+					m_spellcheck[i] = false;
+					return;
+				}
+
+				if ((val > comulativeperc && val <= (comulativeperc + spells[i].perctrigger)) || !spells[i].casttime)
+				{
+					_unit->setAttackTimer(spells[i].attackstoptimer, false);
+					m_spellcheck[i] = true;
+				}
+				comulativeperc += spells[i].perctrigger;
+			}
+		}
+	}
+*/
+protected:
+
+	int nrspells;
+};
 
 // AkamaAI
 
@@ -44,18 +161,20 @@ static Coords toillidan[] =
 	{ 773.819031, 245.568298, 347.780304, 3.509625 },
 	{ 764.204895, 241.937164, 353.667877, 3.454647 },
 	{ 742.981079, 252.640411, 352.996094, 1.444028 },
-	{ 753.753479, 304.675110, 352.997223, 3.092202 }
+	{ 753.753479, 304.675110, 352.997223, 3.092202 },
+	{ 751.941528, 304.626221, 352.996124, 3.128243 }
+	//{ 753.107300, 304.686584, 352.996094, 3.101546 }
 };
 
-class AKAMAAI : public CreatureAIScript
+class AkamaAI : public CreatureAIScript
 {
 public:
-    ADD_CREATURE_FACTORY_FUNCTION(AKAMAAI);
-    AKAMAAI(Creature* pCreature) : CreatureAIScript(pCreature)
+    ADD_CREATURE_FACTORY_FUNCTION(AkamaAI);
+    AkamaAI(Creature* pCreature) : CreatureAIScript(pCreature)
     {
 		m_entry = pCreature->GetEntry();
 
-		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(1, 3000, WALK));
+		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(1, 8000, WALK));
 		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(2, 0, WALK));
         _unit->GetAIInterface()->addWayPoint(CreateWaypoint(3, 0, WALK));
         _unit->GetAIInterface()->addWayPoint(CreateWaypoint(4, 0, WALK));
@@ -66,7 +185,7 @@ public:
         _unit->GetAIInterface()->addWayPoint(CreateWaypoint(9, 0, WALK));
 		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(10, 0, WALK));
 		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(11, 0, WALK));
-		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(12, 5000, WALK));
+		_unit->GetAIInterface()->addWayPoint(CreateWaypoint(12, 0, WALK));
 
 		nrspells = 0;
 		m_spellcheck = new bool[nrspells];
@@ -83,19 +202,17 @@ public:
 		spells[0].perctrigger = 0.0f;
 		spells[0].attackstoptimer = 1000;
 */
-
+		WAY_START = 0;
 		_unit->GetAIInterface()->SetAllowedToEnterCombat(false);
-		_unit->GetAIInterface()->StopMovement(0);
-        _unit->GetAIInterface()->SetAIState(STATE_SCRIPTMOVE);
-		_unit->GetAIInterface()->setMoveType(MOVEMENTTYPE_WANTEDWP);
-		_unit->GetAIInterface()->setWaypointToMove(1);
+		_unit->SetUInt64Value(UNIT_FIELD_FLAGS, U_FIELD_FLAG_UNIT_UNTACKABLE_SELECT);
+		RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
 
     }
     
     void OnCombatStart(Unit* mTarget)
     {
 		CastTime();
-		RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
+		//RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
     }
 
 	void CastTime()
@@ -110,12 +227,12 @@ public:
 			_unit->GetAIInterface()->SetAllowedToEnterCombat(false);
         _unit->GetAIInterface()->setCurrentAgent(AGENT_NULL);
         _unit->GetAIInterface()->SetAIState(STATE_IDLE);
-        RemoveAIUpdateEvent();
+        //RemoveAIUpdateEvent();
     }
 
     void OnDied(Unit * mKiller)
     {
-       RemoveAIUpdateEvent();
+       //RemoveAIUpdateEvent();
 	   delete[] spells;
 	   delete[] m_spellcheck;
 	   spells = NULL;
@@ -124,14 +241,49 @@ public:
 
     void AIUpdate()
     {
-		if (!m_phase)
+		if (m_phase == 0 && !WAY_START)
 		{
-			if (AKAMA_DIALOG == 15)
-			{
-				_unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "We've come to end your reign, Illidan. My people and all of Outland shall be free!");
-				_unit->PlaySoundToSet(10385);
-			}
+			_unit->GetAIInterface()->StopMovement(0);
+			_unit->GetAIInterface()->SetAIState(STATE_SCRIPTMOVE);
+			_unit->GetAIInterface()->setMoveType(MOVEMENTTYPE_WANTEDWP);
+			_unit->GetAIInterface()->setWaypointToMove(1);
+			WAY_START = 1;
 		}
+
+		if (AKAMA_DIALOG)
+		{
+			switch(AKAMA_DIALOG)
+			{
+			case 8:
+			    {
+			        _unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "We've come to end your reign, Illidan. My people and all of Outland shall be free!");
+					_unit->PlaySoundToSet(11389);
+					_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 378);
+			    }break;
+			case 13:
+				{
+					_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 0);
+				}break;
+			case 17:	// (16.5) must check voiding time changing possibility!
+			    {
+			        _unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "The time has come! The moment is at hand!");
+					_unit->PlaySoundToSet(11380);
+					_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 378);
+			    }break;
+			case 19:	// changed from 18, because I don't have weapon model ids yet to add them to function
+				{
+					_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 0);
+					_unit->SetUInt64Value(UNIT_FIELD_FLAGS, 0);
+					_unit->GetAIInterface()->SetAllowedToEnterCombat(true);
+					//_unit->GetAIInterface()->m_moveRun = true;
+					//_unit->GetAIInterface()->MoveTo(714.594788, 305.476044, 353.309204, 3.170653);	// it's just hack to prvent Akama to stand in place (as I have no idea now how to target and attack Illidan =S)
+					_unit->GetAIInterface()->m_canMove = true;
+					Unit * target = _unit->GetMapMgr()->GetInterface()->GetObjectNearestCoords<Creature, TYPEID_UNIT>(22917, 704.539001f, 305.282013f, 353.919006f);	// no idea if this works as I don't have right Akama (hostile with Illidan)
+					_unit->setAttackTarget(target);
+				}break;
+			};
+		}
+
 		if (m_phase)
 		{
 			float val = sRand.rand(100.0f);
@@ -239,13 +391,17 @@ public:
 		        _unit->GetAIInterface()->setWaypointToMove(11);
 			}break;
 		case 11:
-			{
+		    {
 				_unit->GetAIInterface()->setMoveType(MOVEMENTTYPE_WANTEDWP);
-				_unit->GetAIInterface()->setWaypointToMove(12);
+		        _unit->GetAIInterface()->setWaypointToMove(12);
 			}break;
 		case 12:
 			{
 				AKAMA_DIALOG = 1;
+				//_unit->GetAIInterface()->setCurrentAgent(AGENT_NULL);
+				//_unit->GetAIInterface()->SetAIState(STATE_SCRIPTIDLE);	// one of those messes Akama =S
+				_unit->GetAIInterface()->setMoveType(MOVEMENTTYPE_DONTMOVEWP);
+				_unit->GetAIInterface()->setWaypointToMove(0);
 			}break;
 		}
     }
@@ -271,6 +427,7 @@ public:
 
 protected:
 
+	uint32 WAY_START;
     uint32 m_entry;
     uint32 m_currentWP;
     bool *m_spellcheck;
@@ -280,7 +437,7 @@ protected:
 
 // Illidan StormrageAI
 
-#define CN_ILLIDAN_STORMRAGE 22083 // should be 22917
+#define CN_ILLIDAN_STORMRAGE 22917 //22083 // should be 22917
 
 // Normal form spells
 #define SHEAR 41032				// +
@@ -297,6 +454,12 @@ protected:
 // Other spells
 #define SHADOW_PRISON 40647
 
+// Missing Spells (how is it possible with so much spell added already? :O
+// All spells from Phase 2 without Summon Tear of Azzinoth 39855
+
+// Phase 2 spells
+#define FIREBALL 40598	// I took it as id was close to what we hae here and it has blue icon (Illidan used spells with blue fire =P)
+
 // TRANSFORM 40115
 // DEMON TRANSFORM 40511, 40398, 40510
 // Others Maiev/Akama
@@ -310,16 +473,18 @@ protected:
 #define DEMON_FORM2 40398
 #define DEMON_FORM3 40510
 
-// TEAR OF AZZINOTH as summon spell for phase 2 ?
+// TEAR OF AZZINOTH as summon spell for phase 2 ? YES IT IS! Summon Tear of Azzinoth 39855 summons flame of azzinoth 22997
 
 // NOTES: CHECK FLAMES' TEXT AS HE CAN BE HEARD DURING TRANSFORMATIONS SOMETIMES;
-//		  CHECK BLASTS AS THEY ARE USED LITTLE TOO EARLY;
+//		  CHECK BLASTS AS THEY ARE USED LITTLE TOO EARLY BEFORE FULL TRANSED;
 
-class ILLIDANSTORMRAGEAI : public CreatureAIScript
+// OTHERS: Summon Parasitic Shadowfiends 41915; Parasitic Shadowfiend 41914 [41913]
+
+class IllidanStormrageAI : public CreatureAIScript
 {
 public:
-    ADD_CREATURE_FACTORY_FUNCTION(ILLIDANSTORMRAGEAI);
-    ILLIDANSTORMRAGEAI(Creature* pCreature) : CreatureAIScript(pCreature)
+    ADD_CREATURE_FACTORY_FUNCTION(IllidanStormrageAI);
+    IllidanStormrageAI(Creature* pCreature) : CreatureAIScript(pCreature)
     {
 		DemonPhaseTimer = 60;
 		m_phase = 1;	// must be 0 to use it with some other stuff, but it's good for tests.
@@ -399,6 +564,11 @@ public:
 		spells[10].info = sSpellStore.LookupEntry(DEMON_FORM3);
 		spells[10].instant = true;
 
+		spells[11].info = sSpellStore.LookupEntry(FIREBALL);
+		spells[11].instant = false;
+
+		_unit->SetUInt64Value(UNIT_FIELD_FLAGS, U_FIELD_FLAG_UNIT_UNTACKABLE_SELECT);
+		RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
 		_unit->GetAIInterface()->SetAllowedToEnterCombat(false);
 		_unit->GetAIInterface()->m_canMove = false;
 		DemonPhase = 0;
@@ -413,7 +583,7 @@ public:
 		CastTime();
 		_unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "You are not prepared!");	// only this one?
 		_unit->PlaySoundToSet(11466);
-		RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
+		//RegisterAIUpdateEvent(_unit->GetUInt32Value(UNIT_FIELD_BASEATTACKTIME));
     }
 
 	void CastTime()
@@ -451,7 +621,7 @@ public:
 		CastTime();
         _unit->GetAIInterface()->setCurrentAgent(AGENT_NULL);
         _unit->GetAIInterface()->SetAIState(STATE_IDLE);
-        RemoveAIUpdateEvent();
+        //RemoveAIUpdateEvent();
     }
 
     void OnDied(Unit * mKiller)
@@ -460,7 +630,7 @@ public:
 		DemonPhase = 0;
 		m_phase = 1;
 		CastTime();
-       RemoveAIUpdateEvent();
+       //RemoveAIUpdateEvent();
 	   delete[] spells;
 	   delete[] m_spellcheck;
 	   spells = NULL;
@@ -471,51 +641,95 @@ public:
 
 	void AIUpdate()	//Unit *p_time
 	{
-		if (DemonPhase > 0 && DemonPhase < 9)
+		if (AKAMA_DIALOG)
 		{
-			DemonTrans();
-		}
-
-		if (DemonPhase > 9 && DemonPhase < 17)	
-		{
-			TransRevert();
-		}
-
-		else
-		{
-
-			if (m_phase == 1)	// First Phase (without demon form)
+			switch(AKAMA_DIALOG)
 			{
-				PhaseOne();
+			case 1:
+			    {
+			        _unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "Akama... your duplicity is hardly surprising. I should have slaughtered you and your malformed brethren long ago.");
+					_unit->PlaySoundToSet(11463);
+					_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 378);	// EMOTE_STATE_TALK
+			    }break;
+			case 7:		// 8
+				{
+				_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 0);
+				}break;
+			case 13:	// (12.5) I would give it 4.5 diff, but must check if this is possible to manipulate time between two AIUpdate voiding
+			    {
+			        _unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "Boldly said. But I remain unconvinced.");
+					_unit->PlaySoundToSet(11464);
+					_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 378);
+			    }break;
+			case 16:	//17
+				{
+				_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 0);
+				}break;
+			case 18:
+				{
+					_unit->SetUInt64Value(UNIT_FIELD_FLAGS, 0);
+					_unit->GetAIInterface()->SetAllowedToEnterCombat(true);
+					_unit->GetAIInterface()->m_canMove = true;
+				}break;
+			};
+			AKAMA_DIALOG++;
+		}
+
+		if (_unit->isInCombat())
+		{
+			if (DemonPhase > 0 && DemonPhase < 9)
+			{
+				DemonTrans();
 			}
+	
+			if (DemonPhase > 9 && DemonPhase < 17)	
+			{
+				TransRevert();
+			}
+	
+			else
+			{
+	
+				if (m_phase == 1)	// First Phase (without demon form)
+				{
+					PhaseOne();
+				}
+			
+				if (m_phase == 2)	// Second Phase (Fly phase)
+				{
+					PhaseTwo();
+				}
 		
-			if (m_phase == 2) return;	// Second Phase (Fly phase)
+				if (m_phase == 3)	// Third Phase normal form with ability to change into Demon
+				{
+					PhaseThree();
+				}
+		
+				if (m_phase == 4)	// Fourth Phase Demon Form
+				{
+					PhaseFourth();
+				}
 	
-			if (m_phase == 3)	// Third Phase normal form with ability to change into Demon
-			{
-				PhaseThree();
+				if (m_phase == 5) return;	// Fifth Phase with ability to change into Demon and Enrage
 			}
-	
-			if (m_phase == 4)	// Fourth Phase Demon Form
-			{
-				PhaseFourth();
-			}
-
-			if (m_phase == 5) return;	// Fifth Phase with ability to change into Demon and Enrage
 		}
 	}
 
 	void PhaseOne()
-	{/*
+	{
 		if (_unit->GetHealthPct() <= 95)	// to change in future
 		{
+			//_unit->GetAIInterface()->WipeTargetList();
+			//_unit->GetAIInterface()->WipeHateList();
+			if(_unit->GetCurrentSpell() != NULL)
+                _unit->GetCurrentSpell()->cancel();
 			_unit->SetUInt64Value(UNIT_FIELD_FLAGS, U_FIELD_FLAG_UNIT_UNTACKABLE_SELECT);
-			_unit->GetAIInterface()->WipeTargetList();
-			_unit->GetAIInterface()->WipeHateList();
-			_unit->GetAIInterface()->SetAllowedToEnterCombat(false);
-			_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 254);
+			_unit->Emote(EMOTE_ONESHOT_LIFTOFF);
 			_unit->GetAIInterface()->m_moveFly = true;
-			m_phase = 2;
+
+			_unit->GetAIInterface()->SetAllowedToEnterCombat(false);	// he can attack and this is main problem of this phase =S
+			_unit->GetAIInterface()->m_canMove = false;
+				
 			_unit->GetAIInterface()->SetAIState(STATE_SCRIPTIDLE);
             _unit->GetAIInterface()->setMoveType(MOVEMENTTYPE_DONTMOVEWP);
             _unit->GetAIInterface()->setWaypointToMove(0);
@@ -523,12 +737,11 @@ public:
             data << _unit->GetNewGUID();
             data << uint32(0);
             _unit->SendMessageToSet(&data, false);
-		}
-*/
-		if (_unit->GetHealthPct() <= 90)	// it shouldn't be here but in phase 2, but it's good for tests
-		{
-			m_phase = 3;
-			DemonPhaseTimer = 10;
+
+			m_phase = 2;
+
+			FoASummon();
+			FoADeaths = 2;
 		}
 
 		else
@@ -538,8 +751,46 @@ public:
 		}
 	}
 
+	void FoASummon()	// little hacky way to spawn them by pushing into world, but proper spell won't work now
+	{
+		_unit->GetMapMgr()->GetInterface()->SpawnCreature(CN_FLAME_OF_AZZINOTH, 672.039246, 326.748322,
+		354.206390, 0.207343, true, false, 0, 0);
+		_unit->GetMapMgr()->GetInterface()->SpawnCreature(CN_FLAME_OF_AZZINOTH, 673.008667, 283.813660,
+		354.267548, 6.203853, true, false, 0, 0);
+	}
+
 	void PhaseTwo()
 	{
+		if (_unit->GetAIInterface()->GetNextTarget())
+		{
+			Unit *target = NULL;
+			target = _unit->GetAIInterface()->GetNextTarget();
+			//_unit->CastSpell(target, spells[11].info, spells[11].instant);	// UNTESTED YET!
+			_unit->CastSpellAoF(target->GetPositionX(),target->GetPositionY(),target->GetPositionZ(), spells[11].info, spells[11].instant);
+		}
+
+		if (!FoADeaths)
+		{
+			if(_unit->GetCurrentSpell() != NULL)
+                _unit->GetCurrentSpell()->cancel();
+			_unit->SetUInt64Value(UNIT_FIELD_FLAGS, 0);
+			_unit->Emote(EMOTE_ONESHOT_LAND);
+			_unit->GetAIInterface()->m_moveFly = false;
+
+			_unit->GetAIInterface()->SetAllowedToEnterCombat(true);
+			_unit->GetAIInterface()->m_canMove = true;
+/*	// this is for future, because Idk how I will do fire walls (as I don't even know correct spell name =()
+			_unit->GetAIInterface()->SetAIState(STATE_SCRIPTIDLE);
+            _unit->GetAIInterface()->setMoveType(MOVEMENTTYPE_DONTMOVEWP);
+            _unit->GetAIInterface()->setWaypointToMove(0);
+*/
+			WorldPacket data(SMSG_MOVE_UNSET_HOVER, 13);
+			data << _unit->GetNewGUID();
+			data << uint32(0);
+			_unit->SendMessageToSet(&data, false);
+			m_phase = 3;
+			DemonPhaseTimer = 60;
+		}
 	}
 
 	void PhaseThree()
@@ -567,6 +818,8 @@ public:
 
 	void DemonTrans()
 	{
+		if(_unit->GetCurrentSpell() != NULL)
+                _unit->GetCurrentSpell()->cancel();
 		if (DemonPhase == 1)
 		{
 			_unit->SendChatMessage(CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, "Behold the power... of the demon within!");
@@ -594,6 +847,8 @@ public:
 
 	void TransRevert()
 	{
+		if(_unit->GetCurrentSpell() != NULL)
+                _unit->GetCurrentSpell()->cancel();
 		if (DemonPhase == 10)	// 1 diff is too long?
 			//_unit->SetUInt32Value(UNIT_NPC_EMOTESTATE, 403);	// changed to spells as I can't control timer in a way I would like to and without that it doesn't look good
 			_unit->CastSpell(_unit, spells[8].info, spells[8].instant);
@@ -715,6 +970,7 @@ protected:
 
 void SetupBlackTemple(ScriptMgr * mgr)
 {
-	mgr->register_creature_script(CN_AKAMA, &AKAMAAI::Create);
-	mgr->register_creature_script(CN_ILLIDAN_STORMRAGE, &ILLIDANSTORMRAGEAI::Create);
+	mgr->register_creature_script(CN_FLAME_OF_AZZINOTH, &FlameOfAzzinothAI::Create);
+	mgr->register_creature_script(CN_AKAMA, &AkamaAI::Create);
+	mgr->register_creature_script(CN_ILLIDAN_STORMRAGE, &IllidanStormrageAI::Create);
 }
