413375f0d9e49012586f21c3690e52fa93e2f1ae
Zack <Zack@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-07-02 13:14:03 +0000

    maybe this will fix channeled spells. NTY.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2273,14 +2273,12 @@ void Aura::SpellAuraPeriodicTriggerSpell(bool apply)
 		if(!m_caster)
 			return;
 
-
-		Unit *target = (m_target != 0) ? m_target : GetUnitCaster();
-		if(target)
-			sEventMgr.AddEvent(this, &Aura::EventPeriodicTriggerSpell, spe, target->GetGUID(),
+		if(m_caster->GetUInt64Value(UNIT_FIELD_CHANNEL_OBJECT))
+			sEventMgr.AddEvent(this, &Aura::EventPeriodicTriggerSpell, spe, m_caster->GetUInt64Value(UNIT_FIELD_CHANNEL_OBJECT),
+			EVENT_AURA_PERIODIC_TRIGGERSPELL,GetSpellProto()->EffectAmplitude[mod->i], 0);
+		else if(m_target)
+			sEventMgr.AddEvent(this, &Aura::EventPeriodicTriggerSpell, spe, m_target->GetGUID(),
 				EVENT_AURA_PERIODIC_TRIGGERSPELL,GetSpellProto()->EffectAmplitude[mod->i], 0);
-		//have no idea where is channeled object used. i thought eating is an animation or emote. Anyway it can be faked with sending some packet or something
-		else sEventMgr.AddEvent(this, &Aura::EventPeriodicTriggerSpell, spe, m_caster->GetUInt64Value(UNIT_FIELD_CHANNEL_OBJECT),
-			EVENT_AURA_PERIODIC_TRIGGERSPELL,GetSpellProto()->EffectAmplitude[mod->i], 0);	
 	}
 }
 
