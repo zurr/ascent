b6ff6e5b9ab472c6afdcad1f66ecb754503657bd
Burlex <Burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-07-26 10:16:58 +0000

    * License change on remaining files. - Rename.

--- src/antrix/Master.cpp
+++ src/antrix/Master.cpp
@@ -1,3 +1,20 @@
+/****************************************************************************
+ *
+ * General Object Type File
+ * Copyright (c) 2007 Team Ascent
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons,
+ * 543 Howard Street, 5th Floor, San Francisco, California, 94105, USA.
+ *
+ * EXCEPT TO THE EXTENT REQUIRED BY APPLICABLE LAW, IN NO EVENT WILL LICENSOR BE LIABLE TO YOU
+ * ON ANY LEGAL THEORY FOR ANY SPECIAL, INCIDENTAL, CONSEQUENTIAL, PUNITIVE OR EXEMPLARY DAMAGES
+ * ARISING OUT OF THIS LICENSE OR THE USE OF THE WORK, EVEN IF LICENSOR HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
+
 #include "Master.h"
 #include "CConsole.h"
 #include "../shared/CrashHandler.h"
@@ -152,12 +169,8 @@ bool Master::Run(int argc, char ** argv)
 		sLog.m_fileLogLevel = -1;
 		sLog.m_screenLogLevel = 3;
 	}
-
-	sLog.outString("==============================================================================");
+	
 	sLog.outString(BANNER, g_getRevision());
-	sLog.outString("");
-	sLog.outString("Copyright (c) 2007 Antrix Team. This software is under the QPL license, for");
-	sLog.outString("more information look under the COPYING file in this distribution.");
 	sLog.outString("==============================================================================");
 	sLog.outString("");
 	if(do_version)
--- src/logonserver/Main.cpp
+++ src/logonserver/Main.cpp
@@ -1,14 +1,17 @@
 /****************************************************************************
  *
  * General Object Type File
- * Copyright (c) 2007 Antrix Team
+ * Copyright (c) 2007 Team Ascent
  *
- * This file may be distributed under the terms of the Q Public License
- * as defined by Trolltech ASA of Norway and appearing in the file
- * COPYING included in the packaging of this file.
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons,
+ * 543 Howard Street, 5th Floor, San Francisco, California, 94105, USA.
  *
- * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * EXCEPT TO THE EXTENT REQUIRED BY APPLICABLE LAW, IN NO EVENT WILL LICENSOR BE LIABLE TO YOU
+ * ON ANY LEGAL THEORY FOR ANY SPECIAL, INCIDENTAL, CONSEQUENTIAL, PUNITIVE OR EXEMPLARY DAMAGES
+ * ARISING OUT OF THIS LICENSE OR THE USE OF THE WORK, EVEN IF LICENSOR HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
  *
  */
 
@@ -23,17 +26,17 @@
 
 #ifdef WIN32
 #define PLATFORM_TEXT "Win32"
-#define BANNER "Antrix/Win32-2.1.2-%u :: Logon Server                        www.emupedia.com"
+#define BANNER "Ascent/Win32-2.1.2-%u :: Logon Server"
 #else
 #if UNIX_FLAVOUR == UNIX_FLAVOUR_LINUX
 #define PLATFORM_TEXT "Linux"
-#define BANNER "Antrix/Linux-2.1.2-%u :: Logon Server                        www.emupedia.com"
+#define BANNER "Ascent/Linux-2.1.2-%u :: Logon Server"
 #elif UNIX_FLAVOUR == UNIX_FLAVOUR_BSD
 #define PLATFORM_TEXT "FreeBSD"
-#define BANNER "Antrix/FreeBSD-2.1.2-%u :: Logon Server                      www.emupedia.com"
+#define BANNER "Ascent/FreeBSD-2.1.2-%u :: Logon Server"
 #else
 #define PLATFORM_TEXT "Unix"
-#define BANNER "Antrix/Unix-2.1.2-%u :: Logon Server                         www.emupedia.com"
+#define BANNER "Ascent/Unix-2.1.2-%u :: Logon Server"
 #endif
 #include <sched.h>
 #endif
@@ -184,12 +187,8 @@ void LogonServer::Run(int argc, char ** argv)
 		sLog.m_fileLogLevel = -1;
 		sLog.m_screenLogLevel = 3;
 	}
-
-	sLog.outString("==============================================================================");
+	
 	sLog.outString(BANNER, g_getRevision());
-	sLog.outString("");
-	sLog.outString("Copyright (c) 2007 Antrix Team. This software is under the QPL license, for");
-	sLog.outString("more information look under the COPYING file in this distribution.");
 	sLog.outString("==============================================================================");
 	sLog.outString("");
 	if(do_version)
