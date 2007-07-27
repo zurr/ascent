7cbe5debe7140c3efe10191ec9275f365df60199
Burlex <Burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-07-27 16:43:59 +0000

    - missing files

---
+++ src/game/WorkerServerClient.cpp
@@ -0,0 +1,86 @@
+/****************************************************************************
+ *
+ * General Object Type File
+ * Copyright (c) 2007 Team Ascent
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons,
+ * 543 Howard Street, 5th Floor, San Francisco, California, 94105, USA.
+ *
+ * EXCEPT TO THE EXTENT REQUIRED BY APPLICABLE LAW, IN NO EVENT WILL LICENSOR BE LIABLE TO YOU
+ * ON ANY LEGAL THEORY FOR ANY SPECIAL, INCIDENTAL, CONSEQUENTIAL, PUNITIVE OR EXEMPLARY DAMAGES
+ * ARISING OUT OF THIS LICENSE OR THE USE OF THE WORK, EVEN IF LICENSOR HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ */
+
+#include "StdAfx.h"
+#ifdef CLUSTERING
+
+WSClient::WSClient(SOCKET fd) : Socket(fd, 1024576, 1024576)
+{
+	_remaining = 0;
+	_cmd = 0;
+}
+
+WSClient::~WSClient()
+{
+
+}
+
+void WSClient::OnRead()
+{
+	for(;;)
+	{
+		if(!_cmd)
+		{
+			if(GetReadBufferSize() < 6)
+				break;
+
+			Read(2, (uint8*)&_cmd);
+			Read(4, (uint8*)&_remaining);
+		}
+
+		if(_remaining && GetReadBufferSize() < _remaining)
+			break;
+
+		WorldPacket * pck = new WorldPacket(_cmd, _remaining);
+		_cmd = 0;
+		pck->resize(_remaining);
+		Read(_remaining, (uint8*)pck->contents());
+
+		/* we could handle auth here */
+		printf("Got packet from RS: %u\n", pck->GetOpcode());
+		sClusterInterface.QueuePacket(pck);
+	}
+}
+
+void WSClient::OnConnect()
+{
+	sClusterInterface.SetSocket(this);
+}
+
+void WSClient::SendPacket(WorldPacket * data)
+{
+	bool rv;
+	uint32 size = data->size();
+	uint16 opcode = data->GetOpcode();
+	if(!IsConnected())
+		return;
+
+	BurstBegin();
+
+	// Pass the header to our send buffer
+	rv = BurstSend((const uint8*)&opcode, 2);
+	rv = BurstSend((const uint8*)&size, 4);
+
+	// Pass the rest of the packet to our send buffer (if there is any)
+	if(size > 0 && rv)
+		rv = BurstSend((const uint8*)data->contents(), size);
+
+	if(rv) BurstPush();
+	BurstEnd();
+}
+
+#endif
