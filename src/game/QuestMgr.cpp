402900d471c6d38eecd73bc1ae6ccd9651c286c3
Burlex <Burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-08-03 19:12:28 +0000

    - seems there was a missing } else { here.

--- src/game/QuestMgr.cpp
+++ src/game/QuestMgr.cpp
@@ -851,6 +851,7 @@ void QuestMgr::OnQuestFinished(Player* plr, Quest* qst, Object *qst_giver, uint3
 					{
 						plr->GetItemInterface()->BuildInventoryChangeError(NULL, NULL, INV_ERR_INVENTORY_FULL);
 					}
+					else
 					{
 						Item *itm = objmgr.CreateItem(qst->reward_choiceitem[reward_slot], plr);
 						itm->SetUInt32Value(ITEM_FIELD_STACK_COUNT, uint32(qst->reward_choiceitemcount[reward_slot]));
