ad795e2dd6fe9ac6a340d7b1a8be03f490da7281
Zack <Zack@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-08-23 14:12:45 +0000

    added event type "call for help text" for mobs. COPY+PASTE job :P

--- src/game/AIInterface.cpp
+++ src/game/AIInterface.cpp
@@ -1027,6 +1027,8 @@ void AIInterface::_UpdateCombat(uint32 p_time)
 			{
 				FindFriends(400.0f/*20.0f*/);
 				m_hasCalledForHelp = true; // We only want to call for Help once in a Fight.
+				if(m_Unit->GetTypeId() == TYPEID_UNIT)
+						objmgr.HandleMonsterSayEvent(((Creature*)m_Unit), MONSTER_SAY_EVENT_CALL_HELP);
 				CALL_SCRIPT_EVENT(m_Unit, OnCallForHelp)();
 			}break;
 		}
