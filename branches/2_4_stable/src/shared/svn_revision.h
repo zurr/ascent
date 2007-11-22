1a5bbf5051303804a1f53e3b302313c59e478444
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-11-22 23:36:25 +0000

    * branched 2_4_stable

---
+++ branches/2_4_stable/src/shared/svn_revision.h
@@ -0,0 +1,16 @@
+// $Id$
+
+#ifndef _SVN_REVISION_H
+#define _SVN_REVISION_H
+
+static const char * REVISION_TEXT	= "$Rev$";
+static const char * BUILD_TAG		= "2.3-TRUNK";
+
+inline int g_getRevision()
+{
+	const char * p = REVISION_TEXT + 6;
+	return atoi(p);
+}
+
+/////////////////////////////////////////////////////////////
+#endif 
